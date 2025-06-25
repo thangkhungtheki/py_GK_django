1bc883ae3a3f4ef30b4e5c64e1f5fbeae46e12ca
thangkhungtheki <thangkhungtheki@gmail.com>
2025-06-25 11:23:15 +0700

    sua ten

--- gksolutions/gkcentral/apps.py
+++ gksolutions/gkcentral/apps.py
@@ -4,3 +4,5 @@ from django.apps import AppConfig
 class GkcentralConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'gkcentral'
+ # Đây chính là tên hiển thị mà anh muốn đặt
+    verbose_name = 'Gia Khang' 
